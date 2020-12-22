23c55d6b79504f356c6d238edc09e2130a5ef216
salma elgohary <salma.wael101@gmail.com>
2020-12-23 00:20:26 +0200

    Add files via upload
    https://www.urionlinejudge.com.br/judge/en/problems/view/1014

---
+++ Lvl0/C++/Basics/Consumption.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+	int X;
+	double Y;
+	cin >> X;
+	cin >> setprecision(1) >> Y;
+	cout << fixed << setprecision(3) << X/Y << " km/l\n";
+	
+	return 0;
+	
+}
