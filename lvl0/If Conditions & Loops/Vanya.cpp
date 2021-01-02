86dca668c26ac9582b563248e215781a76027c64
salma elgohary <salma.wael101@gmail.com>
2021-01-02 07:02:39 +0200

    Add files via upload

---
+++ lvl0/If Conditions & Loops/Vanya.cpp
@@ -0,0 +1,24 @@
+/* 
+	A. Vanya And Fence  - CODEFORCES 
+*/ 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n,h;
+	int w = 0;
+
+	cin >> n >> h;
+		
+	for (int i = 0; i<n;i++)
+	{
+		int p;
+		cin >> p;
+		if (p > h) w += 2;
+		else w += 1;
+	}
+	
+	cout <<  w <<"\n";	
+}
