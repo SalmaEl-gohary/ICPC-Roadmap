1bb3a4b01e1df4b92f6087f07886a4bf57b53e76
salma elgohary <salma.wael101@gmail.com>
2021-01-02 07:05:37 +0200

    Update BeatTheSpread.cpp

--- lvl0/If Conditions & Loops/BeatTheSpread.cpp
+++ lvl0/If Conditions & Loops/BeatTheSpread.cpp
@@ -1,5 +1,6 @@
 /* 
-	Beat The Spread - online judge 
+    https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1753
+    Beat The Spread! - online judge 
 */
 
 #include <iostream>
