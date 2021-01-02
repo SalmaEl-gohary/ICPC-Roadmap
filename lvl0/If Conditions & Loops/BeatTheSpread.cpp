9b85e03593f1f57d0d34d9a26c621bbe2351e57f
salma elgohary <salma.wael101@gmail.com>
2021-01-02 07:04:51 +0200

    Add files via upload

---
+++ lvl0/If Conditions & Loops/BeatTheSpread.cpp
@@ -0,0 +1,38 @@
+/* 
+	Beat The Spread - online judge 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int sc1,sc2,n;
+	int sum, diff;
+	
+	cin >> n ;
+	for (int i = 0;i<n;i++)
+	{
+		cin >> sum >> diff;
+		
+		if ((sum+diff)%2 != 0)
+		{
+			cout << "impossible\n";
+			continue;
+		}
+		
+		sc1 =  (sum + diff) /2;
+		sc2 =  sum - sc1;
+		
+		if (sc1 < 0 || sc2 < 0)
+			cout << "impossible\n";
+		else 
+			cout << sc1 << " " << sc2 << "\n";
+		
+	}
+	return 0;
+	
+}
+		
+	
